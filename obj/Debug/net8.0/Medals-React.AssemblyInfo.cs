905dd1ace188e4bace7c15e455a7d1ba50de28b1
Andres Minniti <aminnitipaez@my.wctc.edu>
2026-09-23 09:03:08 -0500

    [Publishing on Azure]

--- obj/Debug/net8.0/Medals-React.AssemblyInfo.cs
+++ obj/Debug/net8.0/Medals-React.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medals-React")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca709b6c074f0c2b368afeacba01db8e9a05b98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30692cbcc99b5f7b15bd20e9008a45ee9bac8853")]
 [assembly: System.Reflection.AssemblyProductAttribute("Medals-React")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Medals-React")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
